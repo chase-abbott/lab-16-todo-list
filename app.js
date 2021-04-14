e0faf7dac93cf58e005c502b2f26fe64176fecd4
Daniella Marks <marks.daniella@gmail.com>
2021-04-14 16:05:01 -0700

    add eventlistener to login

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-import { getLoggedIn, setLoggedIn } from "./local-storage.utils.js";
+import { createUser, isLoggedIn, setLoggedIn } from "./local-storage.utils.js";
 
-setLoggedIn('chase');
 
-console.log(getLoggedIn());
+
+isLoggedIn()
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    console.log(username, password);
+
+    if (usernamePwordMatch(username, password)) {
+        setLoggedIn(username);
+        window.location = './todo';
+        //if they don't match: 
+    } else {
+        alert('Wrong username or password.') 
+        window.location = './todo';
+    
+
+});
+
+createUser(username, password)
--- local-storage.utils.js
+++ local-storage.utils.js
@@ -1,5 +1,6 @@
 
 const LOGGED_IN = 'LOGGED_IN';
+const USER = 'USER'
 
 export function setLoggedIn(username) {
     const stringyUsername = JSON.stringify(username);
@@ -16,10 +17,42 @@ export function getLoggedIn() {
     return parsedLogIn;
 }
 
-export function isLoggedIn(username) {
+export function isLoggedIn() {
     const login = getLoggedIn();
 
-    if (login === username) return true;
+    if (login) return true;
+    
+}
 
-    return false;
+export function setUser(user) {
+    const stringyUser = JSON.stringify(user);
+
+    localStorage.setItem(USER, stringyUser)
+}
+
+export function getUser() {
+    const user = localStorage.getItem(USER);
+    const parsedUser = JSON.parse(user);
+
+    if (!parsedUser) return {};
+
+    return parsedUser;
+}
+
+export function usernamePwordMatch(username, password) {
+    const user = getUser();
+
+    if (username === user.username && password === user.password) {
+        return true;
+    }
+}
+
+export function createUser(username, password) {
+    const user = {
+        username, 
+        password,
+        todos: []
+    };
+
+    setUser(user);
 }
