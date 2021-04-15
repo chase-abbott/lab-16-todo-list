cfa72848f27e61723f4f04c687973dbde66c6295
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-04-15 10:58:42 -0700

    functions for todo

--- local-storage.utils.js
+++ local-storage.utils.js
@@ -1,6 +1,6 @@
 
 const LOGGED_IN = 'LOGGED_IN';
-const USER = 'USER'
+const USER = 'USER';
 
 export function setLoggedIn(username) {
     const stringyUsername = JSON.stringify(username);
@@ -64,8 +64,19 @@ export function doesUserExist(username) {
 
 }
 
+export function completeTodo(todoMessage) {
+    const user = getUser();
+    const matchingTodo = user.todos.find(todo => {
+        console.log(todoMessage, todo.todo);
+        todo.todo === todoMessage;
+    });
+
+    matchingTodo.completed = true;
+    setUser(user);
+}
+
 // export function addTodo(todo) {
 //     const user = getUser();
 
-}
+// }
 
--- render-utils.js
+++ render-utils.js
@@ -1,4 +1,4 @@
-import { getUser, setUser } from './local-storage.utils.js';
+import { getUser, setUser, completeTodo } from './local-storage.utils.js';
 
 export function renderLi(todoObject) {
     const li = document.createElement('li');
@@ -7,73 +7,22 @@ export function renderLi(todoObject) {
 
     li.addEventListener('click', () => {
         li.style.textDecoration = 'line-through';
-        const user = getUser();
-        
-        const matchingTodo = user.todos.find(todo => {
-            
-        })
-    
+
+        completeTodo(todoObject.todo);
     });
 
-    return li
+    return li;
 }
 
+export function renderTodos() {
+    const ul = document.querySelector('ul');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export function renderTodos() {
-
-//     const ul = document.querySelector('ul');
-//     const user = getUser();
-
-//     user.todos.forEach(todo => {
-//         console.log(todo)
-//         console.log(todo.completed)
-//         const li = document.createElement('li');
-//         li.textContent = todo.todo;
-//         if (todo.completed) {
-//             li.style.textDecoration = 'line-through';
-//         }
-//         li.addEventListener('click', () => {
-//             li.style.textDecoration = 'line-through';
-//             todo.completed = true;
-//             console.log(todo.completed)
-
-//             setUser(user);
-//             console.log(user)
-//         })
-//         ul.append(li);
-//     }
-//     );
-//     return ul;
-// }
-
-// export function renderLi(todo) {
-//     const li = document.createElement('li');
-//     const user = getUser();
-
-//     li.textContent = todo.todo;
-
-//     li.addEventListener('click', () => {
-//         li.style.textDecoration = 'line-through';
-//         todo.completed = true;
-//         setUser(user);
-//     });
-
-//     return li;
-// }
-
+    ul.textContent = '';
+    const user = getUser();
+    user.todos.forEach(todo => {
+        const li = document.createElement('li');
+        li.textContent = todo.todo;
+        ul.append(li);
+    });
+}
 
